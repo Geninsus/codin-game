1aefe785057e48cb60478ceb4bec9fec8498ed09
Neabfi <fabien.delattre@outlook.com>
2016-02-06 15:29:14 +0100

    Add Phaser basic template

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,20 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
-    cssnano = require('gulp-cssnano');
-    autoprefixer = require('gulp-autoprefixer');
-    htmlmin = require('gulp-htmlmin');
-    imagemin = require('gulp-imagemin');
-    clean = require('gulp-clean');
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
+    htmlmin = require('gulp-htmlmin'),
+    imagemin = require('gulp-imagemin'),
+    clean = require('gulp-clean'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish');
 
-gulp.task('default', ['clean'], function () {
+
+gulp.task('default', ['lint'],function() {
+
+});
+
+gulp.task('dist', ['clean'], function () {
 
   gulp.src('src/img/**/*')
 		.pipe(imagemin())
@@ -16,13 +23,19 @@ gulp.task('default', ['clean'], function () {
 
   return gulp.src('src/*.html')
       .pipe(useref())
-      .pipe(gulpif(['*.js', '!js/lib/**'], uglify()))
+      .pipe(gulpif('*.js', uglify()))
       .pipe(gulpif('*.css', autoprefixer({
         browsers: ['last 2 versions'],
 			  cascade: false })))
       .pipe(gulpif('*.css', cssnano()))
       .pipe(gulpif('*.html', htmlmin({collapseWhitespace: true})))
       .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('src/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter(stylish));
 });
 
 gulp.task('clean', function () {

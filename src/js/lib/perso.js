ffe1e09d2f9be2bd4fd17e92c19b31d10ee86674
Guillaume <guillaume.courmont@isen-lille.fr>
2016-02-27 19:25:20 +0100

    Update perso.js : getter, syntax modified

--- src/js/lib/perso.js
+++ src/js/lib/perso.js
@@ -10,7 +10,7 @@ var spriteInfo = {
 	memory: {
 		size:{
 			width:500,
-			height:300
+			height:500
 		},
 		position:{
 			x:100,
@@ -27,28 +27,40 @@ var spriteInfo = {
 
 var Perso = function(){};
 /* Constructor of perso with coordinate and the sprite of the perso */ 
-function Perso(x,y,sprite){
-	this.x = x;
-	this.y = y;
+function Perso(sprite){
+	this.x = sprite.x;
+	this.y = sprite.y;
 	this.sprite = sprite;
 	console.log("New perso created at x : " + this.x + " and y : " + this.y + " position.");
 }
+/* Return the X position of the sprite */
+Perso.prototype.getX = function(){
+	return this.x;
+}
+/* Return the Y position of the sprite */
+Perso.prototype.getY = function(){
+	return this.y;
+}
 /* Move to a specific location x and y */ 
 Perso.prototype.moveTo = function(xTarget,yTarget){
-	game.add.tween(sprite).to({ x: xTarget, y: yTarget }, this.velocity, Phaser.Easing.Linear.None, true);
+	game.add.tween(this.sprite).to({ x: xTarget, y: yTarget }, this.velocity, Phaser.Easing.Linear.None, true);
 }
 /* Move to input location */
 Perso.prototype.intput = function(){
-	game.add.tween(sprite).to({ x: coordSpriteOf.input.x, y: coordSpriteOf.input.y }, this.velocity, Phaser.Easing.Linear.None, true);
+	game.add.tween(this.sprite).to({ x: coordSpriteOf.input.x, y: coordSpriteOf.input.y }, this.velocity, Phaser.Easing.Linear.None, true);
 }
 /* Move to output location */
 Perso.prototype.output = function(){
-	game.add.tween(sprite).to({ x: coordSpriteOf.output.x, y: coordSpriteOf.output.y-sprite.height }, this.velocity, Phaser.Easing.Linear.None, true);
+	game.add.tween(this.sprite).to({ x: coordSpriteOf.output.x, y: coordSpriteOf.output.y-sprite.height }, this.velocity, Phaser.Easing.Linear.None, true);
 }
 /* Move to a specific hut memory */
 Perso.prototype.memory = function(ind){
 	var coordHut = coordOfHut(ind);
-	game.add.tween(sprite).to({ x: coordHut.x-sprite.width/2 + spriteInfo.hutMemory.width/2, y: coordHut.y - sprite.height + spriteInfo.hutMemory.height/2}, this.velocity, Phaser.Easing.Linear.None, true);
+	game.add.tween(this.sprite).to({ x: coordHut.x-sprite.width/2 + spriteInfo.hutMemory.width/2, y: coordHut.y - sprite.height + spriteInfo.hutMemory.height/2}, this.velocity, Phaser.Easing.Linear.None, true);
+}
+/* Return the velocity of the sprite */
+Perso.prototype.getVelocity = function(){
+	return this.velocity;
 }
 /* Set the velocity of ther perso */
 Perso.prototype.setVelocity = function(velValue){

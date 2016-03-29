60a95771f7c48d51c657ebe7a3a4e23a4ca2ea8b
Neabfi <fabien.delattre@outlook.com>
2016-03-29 21:01:27 +0200

    Add Data.inputs

--- src/data/data.js
+++ src/data/data.js
@@ -2,30 +2,18 @@ var data = [
   {
     wording : 'Le but de cet exercice est d\'additionner les entrées deux à deux.',
     memory : [null],
+    inputs : [],
+    outputs : [],
     inputsGenerator : function () {
       // Le nombre d'input est pair et entre 8 et 14
       var nb = Math.floor(Math.random() * (15 - 8) + 8);
-      var inputs = [];
-      for(var i = 0; i<nb; i++) {
+      for(var i = 0; i < nb; i++) {
         // Les nombres sont entre 0 et 99
-        inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
+        this.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
       }
-      var outputs = [];
-      for(var i = 0; i<nb-1; i+=2) {
-        outputs.push(inputs[i]+inputs[i+1])
+      for(var i = 0; i < nb-1; i += 2) {
+        this.outputs.push(this.inputs[i] + this.inputs[i + 1])
       }
-      this.outputs = inputs;
-      return inputs;
-    },
-    outputs : []
-},
-{
-  memory : [1, 0, null, null],
-  inputsGenerator : function () {
-    inputs = ['a', 'b', 'c'];
-    this.outputs = inputs;
-    return inputs;
-  },
-  output : []
-}
+    }
+  }
 ];

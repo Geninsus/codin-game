c0eab2a28a7cad4e9deaf58a6d561261b7759337
Olivier Baurain <baurain.o@gmail.com>
2016-03-30 14:38:51 +0200

    Ajout de la class Item

---
+++ src/js/Item.js
@@ -0,0 +1,11 @@
+
+var Item = {
+
+	sprite : null,
+
+	value : null,
+
+	init : function(value) {
+		
+	}
+}
--- src/js/Player.js
+++ src/js/Player.js
@@ -1,7 +1,18 @@
 
-var Player {
+var Player = {
+
+	sprite : null,
+
 	init: function(game) {
 		this.game = game;
-		this.game.add.sprite(20,20,'player');
+		this.sprite = this.game.add.sprite(20,20,'player');
 	},
-}
+
+	moveTo: function(x,y) {
+		this.game.add.tween(this.sprite).to( { x: x , y: y }, 200, Phaser.Easing.Linear.None, true);
+	},
+
+	take: function(item) {
+
+	}
+};

b642698d3a4d3daf1996f2d263c80de75706a2ba
Neabfi <fabien.delattre@outlook.com>
2016-05-27 10:21:42 +0200

    Change levels

--- src/data/data.js
+++ src/data/data.js
@@ -39,7 +39,115 @@ var data = {
       }
     },
     {
-      wording : 'Le but de cet exercice est de 3 -> 3+2+1, 2->2+1',
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de sortir les entrés incrémentés de 1.',
+      commands : ['inbox', 'outbox', 'jump', 'inc', 'dec', 'copyto', 'copyfrom'],
+      memory : [null,null,null,null],
+      inputsGenerator : function () {
+        data.inputs = [];
+        data.outputs = [];
+        // Le nombre d'input est entre 6 et 10
+        var nb = Math.floor(Math.random() * (11 - 6) + 6);
+        for(var i = 0; i < nb; i++) {
+          // Les nombres sont entre 0 et 80
+          data.inputs.push(Math.floor(Math.random() * (81 - 0) + 0));
+        }
+        for(var i = 0; i < nb; i++) {
+          if(data.inputs[i] != 0) data.outputs.push(data.inputs[i]+1);
+        }
+      }
+    },
+    {
+      wording : 'Le but de cet exercice est de faire la somme des nombres entre 1 et le nombre en entrée. Par exemple si l\'entrée vaut 3, la sortie doit valoir 3 + 2 + 1 donc 6.',
       commands : ['inbox', 'outbox'],
       memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
       inputsGenerator : function () {
@@ -160,121 +268,7 @@ var data = {
     {
       wording : 'Le but de cet exercice est de sortir directement les entrées.',
       commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
-      inputsGenerator : function () {
-        data.inputs = [];
-        data.outputs = [];
-        // Le nombre d'input est entre 6 et 10
-        //var nb = Math.floor(Math.random() * (11 - 6) + 6);
-        var nb = 2;
-        for(var i = 0; i < nb; i++) {
-          // Les nombres sont entre 0 et 99
-          data.inputs.push(Math.floor(Math.random() * (100 - 0) + 0));
-        }
-        for(var i = 0; i < nb; i++) {
-          data.outputs.push(data.inputs[i])
-        }
-      }
-    },
-    {
-      wording : 'Le but de cet exercice est de sortir directement les entrées.',
-      commands : ['inbox', 'outbox'],
-      memory : [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null],
+      memory : [],
       inputsGenerator : function () {
         data.inputs = [];
         data.outputs = [];
